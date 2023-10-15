f1f6eb0599eb7bd2d23eed59ed139cba3d8cec8e
SHREYANK <66244570+bshreyank@users.noreply.github.com>
2023-10-15 14:34:58 +0530

    Implement-show-data

--- src/Components/Create.jsx
+++ src/Components/Create.jsx
@@ -1,47 +1,52 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React, { useState } from 'react'
+import axios from 'axios'
 
 //We will be using useNavigate to go from Create page to Read Page after clicking on Submit button.
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from 'react-router-dom'
 //========================================================================>>>>
 const Create = () => {
   //Now we have to store the data we are creating, thats why we are using useState.
   //setEmail jab aapan pass karte hai onchange mai toh vo data sidha email mai store ho jata hai !!!
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
 
-  const history = useNavigate();
+  const history = useNavigate()
 
   //========================================================================>>>>
-  const header = { "Access-Control-Allow-Origin": "*" };
+  const header = { 'Access-Control-Allow-Origin': '*' }
   //========================================================================>>>>
 
   const handleSubmit = (e) => {
     // very important - prevent Default or the page will automatically reload and data will not pass
-    e.preventDefault();
+    e.preventDefault()
     //console to check if the handleSubmit is working or not?
-    console.log("Handle Submit is working");
+    console.log('Handle Submit is working')
 
     // Axios Syntax below code
-    axios.post("https://6526eec7917d673fd76d3b44.mockapi.io/crud", {
-      name: name,
-      email: email,
-      header //above this handleSubmit button this is defined
-    })
+    axios
+      .post('https://6526eec7917d673fd76d3b44.mockapi.io/crud', {
+        name: name,
+        email: email,
+        header, //above this handleSubmit button this is defined
+      })
 
-    //This is using useNavigate
-    // history("/read");  //dont write this way: 
+      //This is using useNavigate
+      // history("/read");  //dont write this way:
 
-    .then(()=>{
-      history("/read");
-    });
-    
-  };
+      .then(() => {
+        history('/read')
+      })
+  }
   //========================================================================>>>>
   return (
     <>
       <br />
-      <h2>===CREATE===</h2>
+      <div className='d-flex justify-content-between m-2'>
+        <h2>===CREATE===</h2>
+        <Link to="/read">
+          <button className='btn btn-info'>Show Data</button>
+        </Link>
+      </div>
       <br />
       <form>
         {/* === Name === */}
@@ -75,12 +80,12 @@ const Create = () => {
         </button>
       </form>
     </>
-  );
-};
-export default Create;
+  )
+}
+export default Create
 
 //======================================================================>>>>>
- /*
+/*
     Syntax:-
     axios({
 
